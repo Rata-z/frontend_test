8e3badc4ab466e3d1242311d84a3eab50691f328
Maciej <86221441+s22964@users.noreply.github.com>
2024-07-13 11:39:40 +0200

    footer and list

--- client/src/app/layout/footer/footer.component.ts
+++ client/src/app/layout/footer/footer.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatMenuModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  // @ViewChild(MatMenuTrigger)
+  // trigger!: MatMenuTrigger;
+  // someMethod() {
+  //   this.trigger.openMenu();
+  // }
 }
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { ionLogoHtml5 } from '@ng-icons/ionicons';
+//import breakpoints
 
 @Component({
   selector: 'app-header',
@@ -10,4 +11,21 @@ import { ionLogoHtml5 } from '@ng-icons/ionicons';
   styleUrl: './header.component.scss',
   providers: [provideIcons({ ionLogoHtml5 })],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  iconSize: string;
+
+  constructor() {
+    this.iconSize = '2.1rem';
+  }
+
+  ngOnInit(): void {
+    if (window.innerWidth >= 960) this.iconSize = '3rem';
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    window.innerWidth >= 960
+      ? (this.iconSize = '3rem')
+      : (this.iconSize = '2.1rem');
+  }
+}
